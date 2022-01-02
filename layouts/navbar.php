45983a9ce0efd4c976e6f1d402bc2e3fb4991866
MD Rakibul Ahsan <mdrakib76634@gmail.com>
2022-01-02 22:35:34 +0600

    update

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -10,22 +10,22 @@
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Mechanics List</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Accessories
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Car Parts</a></li>
+            <li><a class="dropdown-item" href="#">Bick Parts</a></li>
+            <li><hr class="dropdown-divider">Track & Bus Parts</li>
+            <li><a class="dropdown-item" href="#"></a></li>
           </ul>
         </li>
-        <li class="nav-item">
+        <!-- <li class="nav-item">
           <a class="nav-link disabled">Disabled</a>
-        </li>
+        </li> -->
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
